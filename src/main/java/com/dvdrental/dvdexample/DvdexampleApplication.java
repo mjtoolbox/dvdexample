85ce72086f6afb2adea7c945c6faca6466262f5d
Michael Jo <michael.jo@gmail.com>
2019-06-30 10:52:16 -0700

    initialized

---
+++ src/main/java/com/dvdrental/dvdexample/DvdexampleApplication.java
@@ -0,0 +1,13 @@
+package com.dvdrental.dvdexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdexampleApplication.class, args);
+	}
+
+}
